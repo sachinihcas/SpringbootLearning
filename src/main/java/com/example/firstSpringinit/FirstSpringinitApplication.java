b0c83e7275feff0cef804f42a3551ece17a0e0f3
sachinihcas <sachinagrawal769@gmail.com>
2021-09-14 01:25:37 +0530

    initial commit

--- src/main/java/com/example/firstSpringinit/FirstSpringinitApplication.java
+++ src/main/java/com/example/firstSpringinit/FirstSpringinitApplication.java
@@ -2,12 +2,13 @@ package com.example.firstSpringinit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class FirstSpringinitApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FirstSpringinitApplication.class, args);
+		ConfigurableApplicationContext context = SpringApplication.run(FirstSpringinitApplication.class, args);
 	}
 
 }
