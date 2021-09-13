de1552d60282ed1cca25e62165e5ea7d95705af7
sachinihcas <sachinagrawal769@gmail.com>
2021-09-14 01:19:19 +0530

    some changes

---
+++ src/main/java/com/example/firstSpringinit/FirstSpringinitApplication.java
@@ -0,0 +1,13 @@
+package com.example.firstSpringinit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringinitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringinitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/firstSpringinit/FirstSpringinitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.firstSpringinit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringinitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
